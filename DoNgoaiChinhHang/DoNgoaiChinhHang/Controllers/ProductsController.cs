73b6c0ab323bd7d41b456e73eb6aa39ac81bfd09
ptthe075 <phamtrungthe075@gmail.com>
2021-08-23 08:52:03 +0700

    Chi tiet san pham

---
+++ DoNgoaiChinhHang/DoNgoaiChinhHang/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using DoNgoaiChinhHang.Areas.Admin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoNgoaiChinhHang.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private DBContext db = new DBContext();
+        // GET: Products
+        [HttpGet]
+        public ActionResult Detail(string productID)
+        {
+            if (productID == null)
+            {
+                productID = "SP1";
+            }
+            var product = db.Products.Where(p => p.ProductID == productID).FirstOrDefault();
+            var category = db.Categories.Where(c => c.CategoryID == product.CategoryID).FirstOrDefault();
+            var categoryBase = db.CategoryBases.Where(c => c.CategoryBaseID == category.CategoryBaseID).FirstOrDefault();
+            var listRelatedProducts = db.Products.Where(p => p.Brand == product.Brand).Take(4);
+            var listTopProducts = db.Products.Where(p => p.Brand == product.Brand).Take(5);
+
+            ViewBag.categoryName = category;
+            ViewBag.categoryBaseName = categoryBase;
+            ViewBag.listRelatedProducts = listRelatedProducts.Where(p => p.ProductID != productID);
+            ViewBag.listTopProducts = listTopProducts.Where(p => p.ProductID != productID);
+
+            return View(product);
+        }
+    }
+}
