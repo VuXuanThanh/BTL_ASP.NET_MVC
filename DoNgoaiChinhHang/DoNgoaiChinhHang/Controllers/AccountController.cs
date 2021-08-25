84934af545fe975c0497b0bc26c4ce2724b24e25
Thành Vũ <vuxuanthanh2k@gmail.com>
2021-08-25 07:49:06 +0700

    [fix] errors of login and register features

--- DoNgoaiChinhHang/DoNgoaiChinhHang/Controllers/AccountController.cs
+++ DoNgoaiChinhHang/DoNgoaiChinhHang/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,7 +11,10 @@ namespace DoNgoaiChinhHang.Controllers
     public class AccountController : Controller
     {
         private DBContext db = new DBContext();
-        public const string LoginSession = "account";
+        public const string NameSession = "UrNameSS";
+        public const string AddressSession = "AddrSS";
+        public const string PhoneSession = "PhoneSS";
+        public const string IDAccSession = "IDUrSS";
 
         #region Account register and login for users
         [HttpGet]
@@ -22,7 +26,7 @@ namespace DoNgoaiChinhHang.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Register(Account acc)
         {
-            var account = db.Accounts.FirstOrDefault(s => s.Email == acc.Email || s.Phone==acc.Phone);
+            var account = db.Accounts.FirstOrDefault(s => s.Email == acc.Email || s.Phone == acc.Phone);
             if (ModelState.IsValid)
             {
 
@@ -34,7 +38,7 @@ namespace DoNgoaiChinhHang.Controllers
                     acc.Status = true;
                     db.Accounts.Add(acc);
                     db.SaveChanges();
-                    return RedirectToAction("LoginUser","Account");
+                    return RedirectToAction("LoginUser", "Account");
                 }
                 else
                 {
@@ -61,8 +65,11 @@ namespace DoNgoaiChinhHang.Controllers
                     bool verified = BCrypt.Net.BCrypt.Verify(Password, acc.Password.Trim());
                     if (verified)
                     {
-                        Session[LoginSession] = acc;
-                        return RedirectToAction("Index","Home");
+                        Session[NameSession] = acc.CustomerName;
+                        Session[AddressSession] = acc.Address;
+                        Session[PhoneSession] = acc.Phone;
+                        Session[IDAccSession] = acc.AccountID;
+                        return RedirectToAction("Index", "Home");
                     }
                     else
                     {
@@ -78,10 +85,41 @@ namespace DoNgoaiChinhHang.Controllers
             }
             return View();
         }
-        public JsonResult Show()
+        public JsonResult GetInforIntoOrder()
         {
-            var usr = (Account)Session[LoginSession];
-            return Json(usr, JsonRequestBehavior.DenyGet);
+            var indentity = Session[IDAccSession];
+            if (indentity != null)
+            {
+                var usr = new
+                {
+                    Name = Session[NameSession].ToString(),
+                    Phone = Session[PhoneSession].ToString(),
+                    Address = Session[AddressSession].ToString()
+                };
+                return Json(usr, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(false, JsonRequestBehavior.DenyGet);
+            }
+        }
+        public ActionResult Logout()
+        {
+            Session[IDAccSession] = null;
+            Session[NameSession] = null;
+            Session[PhoneSession] = null;
+            Session[AddressSession] = null;
+            return RedirectToAction("LoginUser", "Account");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+
         }
         #endregion
     }
